71070f609f281e0477c33b08966c2036e3e89f4a
comavius <comavius@proton.me>
2025-07-15 14:42:16 +0900

    add include test for clang++

--- compilers/clang/default.nix
+++ compilers/clang/default.nix
@@ -1,14 +1,15 @@
 {pkgs}: let
   myClang = pkgs.clang;
-  /*
   myBoost = pkgs.boost;
   myGmp = pkgs.gmp;
   myEigen = pkgs.eigen;
   myAcLibrary = pkgs.ac-library;
   myZ3 = pkgs.z3;
-  */
   clangWrapper = pkgs.writeShellScriptBin "clang++" ''
-    exec "${myClang}/bin/clang++" "$@" -std=c++23
+    export LD_LIBRARY_PATH="${myBoost}/lib:${myGmp}/lib:${myEigen}/share:${myZ3.lib}/lib:$LD_LIBRARY_PATH"
+    export LIBRARY_PATH="${myBoost}/lib:${myGmp}/lib:${myEigen}/share:${myZ3.lib}/lib:$LIBRARY_PATH"
+    export CPLUS_INCLUDE_PATH="${myBoost.dev}/include:${myGmp.dev}/include:${myEigen}/include:${myAcLibrary.dev}/include:${myZ3.dev}/include:$CPLUS_INCLUDE_PATH"
+    exec "${myClang}/bin/clang++" "$@" -lgmpxx -lgmp -lz3
   '';
   compileCmd = cfg: pkgs.writeShellScriptBin "compile-clang" ''
     ${pkgs.coreutils}/bin/cp ${cfg.src} ${cfg.temp}/main.cpp && \
@@ -26,12 +27,12 @@ in
             run = cfg: "${cfg.out}/main.out";
           };
           tests = {
-            /*
             test1 = {
-              source = builtins.readFile ./main.cpp;
+              source = builtins.readFile ./test1.cpp;
               stdin = "";
-              stdout = "Hello, World!";
+              stdout = builtins.readFile ./test1.stdout;
             };
+            /*
             */
             minimal = {
               source = builtins.readFile ./minimal.cpp;
