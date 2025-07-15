5e0ac8ac5f226b989c1f28dcde273c265f6b727b
comavius <comavius@proton.me>
2025-07-15 14:36:26 +0900

    ✅ add minimal case for clang++ test

--- compilers/clang/default.nix
+++ compilers/clang/default.nix
@@ -1,26 +1,44 @@
 {pkgs}: let
   myClang = pkgs.clang;
+  /*
   myBoost = pkgs.boost;
   myGmp = pkgs.gmp;
   myEigen = pkgs.eigen;
   myAcLibrary = pkgs.ac-library;
   myZ3 = pkgs.z3;
+  */
+  clangWrapper = pkgs.writeShellScriptBin "clang++" ''
+    exec "${myClang}/bin/clang++" "$@" -std=c++23
+  '';
+  compileCmd = cfg: pkgs.writeShellScriptBin "compile-clang" ''
+    ${pkgs.coreutils}/bin/cp ${cfg.src} ${cfg.temp}/main.cpp && \
+    ${clangWrapper}/bin/clang++ -std=c++23 -o ${cfg.out}/main.out ${cfg.temp}/main.cpp
+  '';
 in
-  pkgs.writeShellScriptBin "clang++" ''
-    export LD_LIBRARY_PATH="${myBoost}/lib:${myGmp}/lib:${myEigen}/share:${myZ3.lib}/lib:$LD_LIBRARY_PATH"
-    export LIBRARY_PATH="${myBoost}/lib:${myGmp}/lib:${myEigen}/share:${myZ3.lib}/lib:$LIBRARY_PATH"
-    export CPLUS_INCLUDE_PATH="${myBoost.dev}/include:${myGmp.dev}/include:${myEigen}/include:${myAcLibrary.dev}/include:${myZ3.dev}/include:$CPLUS_INCLUDE_PATH"
-    exec "${myClang}/bin/clang++" "$@" -lgmpxx -lgmp -lz3
-  ''
-  // {
+  clangWrapper // {
     traojudge = {
       languages = [
         {
-          binName = "clang++";
-          compile = cfg: "${pkgs.coreutils}/bin/cp ${cfg.src} ${cfg.temp}/main.cpp && \
-            ${myClang}/bin/clang++ -std=c++23 -o ${cfg.out} ${cfg.temp}/main.cpp -lgmpxx -lgmp -lz3";
-          name = "C++(clang)";
-          run = cfg: "exec ${cfg.out}";
+          main = {
+            binName = "clang++";
+            compile = cfg: "${compileCmd cfg}/bin/compile-clang";
+            name = "C++(clang)";
+            run = cfg: "${cfg.out}/main.out";
+          };
+          tests = {
+            /*
+            test1 = {
+              source = builtins.readFile ./main.cpp;
+              stdin = "";
+              stdout = "Hello, World!";
+            };
+            */
+            minimal = {
+              source = builtins.readFile ./minimal.cpp;
+              stdin = "";
+              stdout = "";
+            };
+          };
         }
       ];
     };
